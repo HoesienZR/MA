0f9864ff3adea8277f83e6108bb6003f12ca87d2
HosseinZFR <ghost.zr4848@gmail.com>
2024-04-22 13:17:47 +0330

    creating like and dislike for posts

--- social/views.py
+++ social/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render,get_object_or_404
-from .models import Post
+
+from account.models import SocialUser
 from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
+
+from .models import Post
 # Create your views here.
 
 
@@ -18,26 +23,24 @@ def show_details(request):
     return render(request, 'views/post_details.html', context=context)
 
 
+@login_required
+@require_POST
 def update_like(request):
     print('enterd this shit')
-    post_id = request.GET.get('post_id')
-    print(post_id)
-    user = request.user
-    if post_id is not None:
-        post = get_object_or_404(Post, id=post_id)
-        print(post.likes.all())
-        if user is post.likes.all():
-            post.likes.remove(user)
-            liked = False
+    if request.method == 'POST':
+        post_id = request.POST.get('item_id')
+        user = request.user
+        if post_id is not None:
+            post = get_object_or_404(Post, id=post_id)
+            if user in post.likes.all():
+                post.likes.remove(user)
+            else:
+                post.likes.add(user)
+            post_likes = post.likes.count()
+            response_data = {
+                'post_likes': post_likes,
+                'success': True,
+            }
         else:
-            post.likes.add(user)
-            liked = True
-        post_likes = post.likes.count()
-        response_data = {
-            'post_likes': post_likes,
-            'liked': liked,
-        }
-    else:
-        response_data = {'error': 'invalid post'}
-    print(response_data)
-    return JsonResponse(response_data,)
+            response_data = {'error': 'invalid post', 'success': False}
+        return JsonResponse(response_data,)
